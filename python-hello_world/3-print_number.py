913109def79708af7389f9b363f05f5fd1af0071
SaLaam17 <slaamri@yahoo.fr>
2025-01-13 15:47:44 +0100

    3-print_number.py

---
+++ python-hello_world/3-print_number.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+number = 98
+print(f"{98} Battery street")
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+#The output of the script should be:
+#the number, followed by Battery street,
+#followed by a new line
+#You are not allowed to cast the variable number into a string
+#Your code must be 3 lines long
+#You have to use f-strings tips
