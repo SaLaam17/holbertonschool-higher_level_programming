43c70314c2498c3b05949144db4a7bb4211d1ce7
SaLaam17 <slaamri@yahoo.fr>
2025-01-16 10:02:50 +0100

    9-print_last_digit.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-for i in range(10):
+for i in range(9):
     for j in range(i + 1, 10):
         if i == 8:
             print("{}{}".format(i, j))
-        elif i < 9 or j < 9:
+        else:
             print("{}{}, ".format(i, j), end="")
