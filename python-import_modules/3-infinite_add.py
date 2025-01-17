c86998105e52a25a44b66998347e18c7e9deeb99
SaLaam17 <slaamri@yahoo.fr>
2025-01-17 11:14:56 +0100

    3-infinite_add.py

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+
+def main():
+    argv = sys.argv[1:]
+    total = 0
+    for arg in argv:
+        total += int (arg)
+    print(total)
+
+
+if __name__ == "__main__":
+    main()
