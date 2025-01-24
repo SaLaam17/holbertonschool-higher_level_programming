01dbc827f37f8dac7d17a135d6761d44a351007a
SaLaam17 <slaamri@yahoo.fr>
2025-01-24 13:56:52 +0100

    0-add_integer.py 0-add_integer.txt

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -8,13 +8,15 @@ def add_integer(a, b=98):
 
     """
     """
-    if not isinstance(a, int):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer or a float")
-    if not isinstance(b, int):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer or a float")
+
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
 
     sum = a +b
     return sum
-
-if __name__ == "__main__":
-    doctest.testfile("0-add_integer.txt")
