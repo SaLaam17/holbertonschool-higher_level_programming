d6924a70ea6b83f22bdb3ace8390c1ea1ce9fd2e
SaLaam17 <slaamri@yahoo.fr>
2025-01-24 13:43:17 +0100

    0-add_integer.py 0-add_integer.txt

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,23 +2,19 @@
 """
 Return the sum of two integers or floats.
 """
-
+import doctest
 
 def add_integer(a, b=98):
 
     """
     """
     if not isinstance(a, int):
-        raise TypeError("a must be an integer")
+        raise TypeError("a must be an integer or a float")
     if not isinstance(b, int):
-        raise TypeError("b must be an integer")
-    if not isinstance(a, float):
-        raise TypeError("a must be a float")
-    if not isinstance(b, int):
-        raise TypeError("b must be an float")
+        raise TypeError("b must be an integer or a float")
 
-    return
+    sum = a +b
+    return sum
 
 if __name__ == "__main__":
-    import doctest
-doctest.testfile("0-add_integer.txt")
+    doctest.testfile("0-add_integer.txt")
