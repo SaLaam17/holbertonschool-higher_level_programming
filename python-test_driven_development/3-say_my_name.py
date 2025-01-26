15deb290a567c1be1704f0086348926690fec9bf
SaLaam17 <slaamri@yahoo.fr>
2025-01-26 16:36:42 +0100

    3-say_my_name.py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -18,6 +18,8 @@ def say_my_name(first_name, last_name=""):
 
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
+    if first_name == "":
+        raise ValueError("first_name must be provided")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
