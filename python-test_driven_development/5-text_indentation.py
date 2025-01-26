1f5a91762e3f10eb621e8d7526edbeaabfbd33ea
SaLaam17 <slaamri@yahoo.fr>
2025-01-26 19:37:25 +0100

    5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -16,19 +16,13 @@ def text_indentation(text):
 
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-
-    chars = ['.', '?', ':']
+    chars = ['.', '?', ':'] 
     result = ""
-    i = 0
 
-    while i < len(text):
-        result += text[i]
-        if text[i] in chars:
-            result += "\n\n"
-            i += 1
-            while i < len(text) and text[i] == ' ':
-                i += 1
-            continue
-        i += 1
-
-    print(result.strip())
+    for char in text:
+        result += char
+        if char in chars:
+            print(result.strip(), end="\n\n")
+            result = ""
+    if result.strip():
+        print(result.strip(), end="")
