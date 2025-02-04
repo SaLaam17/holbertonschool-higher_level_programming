04b92612f82dd57bf3b5d45fde184decfefa1336
SaLaam17 <slaamri@yahoo.fr>
2025-02-04 15:51:48 +0100

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module defines class named BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    represent a class named BaseGeometry
+    """
+    def area(self):
+        """
+        Public instance method
+        """
+        raise Exception("area() is not implemented")
