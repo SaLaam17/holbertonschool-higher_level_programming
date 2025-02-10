a98c2659bd78fa62bc7c46635901ed7b03b40b59
SaLaam17 <slaamri@yahoo.fr>
2025-02-10 11:58:35 +0100

    2-append_write.py

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module documentation.
+"""
+def write_file(filename="", text=""):
+    """
+    Function that writes a string to a text file (UTF8),
+    and returns the number of characters written:
+    """
+    with open(filename,'w', encoding="utf-8") as file:
+        return file.write(text)
