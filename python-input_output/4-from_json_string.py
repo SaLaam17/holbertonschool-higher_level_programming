ac0506612bf1fc18397db0af341181d0687e7f32
SaLaam17 <slaamri@yahoo.fr>
2025-02-10 14:04:00 +0100

    4-from_json_string.py

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module documentation.
+"""
+import json
+
+def from_json_string(my_str):
+    """
+    Function that returns an object (Python data structure)
+    represented by a JSON string.
+    """
+    return json.loads(my_str)
