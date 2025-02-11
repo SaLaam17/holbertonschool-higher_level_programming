36b4549caf319889b444154f72a16e7d0c5c42bb
SaLaam17 <slaamri@yahoo.fr>
2025-02-11 15:41:04 +0100

    docstrings missing for 9-student.py

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """
+./11-main.py student.json
 """
 
 
 class Student:
     """
+    class Student that defines a student.
     """
 
     def __init__(self, first_name, last_name, age):
