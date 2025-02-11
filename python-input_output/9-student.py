6b2dc2bf9eaf043cfb2e8bc324c37f6c06a781a5
SaLaam17 <slaamri@yahoo.fr>
2025-02-11 15:32:32 +0100

    11-student.py

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -9,7 +9,7 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         """
-        Instantiation
+        IInstantiation with first_name, last_name, and age.
         """
         self.first_name = first_name
         self.last_name = last_name
