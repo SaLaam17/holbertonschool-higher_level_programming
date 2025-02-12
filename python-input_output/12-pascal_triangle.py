fcb4acd1f3f588e7349ad312e1601475c2fc719f
SaLaam17 <slaamri@yahoo.fr>
2025-02-12 20:31:42 +0100

    12-pascal_triangle.py

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -2,26 +2,28 @@
 """
 Module
 """
+
+
 def pascal_triangle(n):
     """
     Function that returns a list of lists of integers
     representing the Pascal’s triangle of n.
     """
     if n <= 0:
-        return [] 
-    #list_1 = [1]
-    #list_2 = [list_1[0], list_1[0]]
-    #list_3 = [list_2[0], list_2[0] + list_2[1], list_2[0]]
-    #list_4 = [list_3[0], list_3[0] + list_3[1], list_3[1] + list_3[2], list_3[0]]
-    #list_5 = [list_4[0], list_4[0] + list_4[1], list_4[1] + list_4[2], list_4[2] + list_4[3], list_4[0]]
-
-
-    #list_1 = [1]
-    #list_2 = [list_1[0], list_1[0]]
-    #list_3 = [list_1[0], 2 * list_1[0], list_1[0]]
-    #list_4 = [list_1[0], 3 * list_1[0], 3 * list_1[0], list_1[0]]
-    #list_5 = [list_1[0], 4 * list_1[0], 6 * list_1[0], 4 * list_1[0], list_1[0]]
-    #triangle = [list_1, list_2]
+        return []
+# list_1 = [1]
+# list_2 = [list_1[0], list_1[0]]
+# list_3 = [list_2[0], list_2[0] + list_2[1], list_2[0]]
+# list_4 = [list_3[0], list_3[0] + list_3[1], list_3[1] + list_3[2], list_3[0]]
+# list_5 = [list_4[0], list_4[0] + list_4[1],
+# list_4[1] + list_4[2], list_4[2] + list_4[3], list_4[0]]
+    # list_1 = [1]
+    # list_2 = [list_1[0], list_1[0]]
+    #  list_3 = [list_1[0], 2 * list_1[0], list_1[0]]
+    # list_4 = [list_1[0], 3 * list_1[0], 3 * list_1[0], list_1[0]]
+    # list_5 = [list_1[0], 4 * list_1[0],
+    # 6 * list_1[0], 4 * list_1[0], list_1[0]]
+    # triangle = [list_1, list_2]
 
     triangle = [[1]]
 
