8505722bb283475b42fb355c809c95112f94e984
SaLaam17 <slaamri@yahoo.fr>
2025-02-20 11:21:49 +0100

    task_04_flask.py

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -67,7 +67,7 @@ def add_user():
         return jsonify({"error": "Username is required"}), 400
 
     users[username] = new_user
-    return jsonify({"message": "User added"}), 201
+    return jsonify({"message": "User added", "user": new_user}), 201
 
 
 if __name__ == "__main__":
