e3d27360b5b86921313096931e7c29e9b6a29883
SaLaam17 <slaamri@yahoo.fr>
2025-02-21 16:27:15 +0100

    task_05_basic_security.py

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -99,26 +99,41 @@ def admin_only():
 
 @jwt.unauthorized_loader
 def handle_unauthorized_error(err):
+    """
+    Error handling
+    """
     return jsonify({"error": "Missing or invalid token"}), 401
 
 
 @jwt.invalid_token_loader
 def handle_invalid_token_error(err):
+    """
+    Error handling
+    """
     return jsonify({"error": "Invalid token"}), 401
 
 
 @jwt.expired_token_loader
 def handle_expired_token_error(err):
+    """
+    Error handling
+    """
     return jsonify({"error": "Token has expired"}), 401
 
 
 @jwt.revoked_token_loader
 def handle_revoked_token_error(err):
+    """
+    Error handling
+    """
     return jsonify({"error": "Token has been revoked"}), 401
 
 
 @jwt.needs_fresh_token_loader
 def handle_needs_fresh_token_error(err):
+    """
+    Error handling
+    """
     return jsonify({"error": "Fresh token required"}), 401
 
 
