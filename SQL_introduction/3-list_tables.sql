058083573dc40836356a46fcf8d30aa52767eef9
SaLaam17 <slaamri@yahoo.fr>
2025-03-03 15:18:01 +0100

    fixing comments syntax errors

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,3 +1,3 @@
---Script that lists all the tables of a database in my MySQL server.
---Query that lists all the tables of a database in my MySQL server.
+-- Script that lists all the tables of a database in my MySQL server.
+-- Query that lists all the tables of a database in my MySQL server.
 SHOW TABLES;
