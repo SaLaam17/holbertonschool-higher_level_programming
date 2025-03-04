621c61b4594be5941cffd61983be0404d0be43b3
SaLaam17 <slaamri@yahoo.fr>
2025-03-04 18:15:38 +0100

    2-create_read_user.sql

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -4,4 +4,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Query that creates user_0d_2 with password set to user_0d_2_pwd.
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- Query that set only SELECT privilege in the database hbtn_0d_2 for user_0d_2. 
-GRANT SELECT ON *.* TO 'user_0d_1'@'localhost';
+GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
