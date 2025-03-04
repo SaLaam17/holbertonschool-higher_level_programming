a7f2e5973265d88b1eaac77382877f8230c1470c
SaLaam17 <slaamri@yahoo.fr>
2025-03-04 19:10:15 +0100

    6-states.sql

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -2,4 +2,4 @@
 -- Query that creates the database hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- Query that creates the table states.
-CREATE TABLE IF NOT EXISTS states (id INT PRIMARY KEY, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS states (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
