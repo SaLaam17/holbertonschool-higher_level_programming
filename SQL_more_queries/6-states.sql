7fd31d40c878c8c6698df7b0dfe6338c90718c9e
SaLaam17 <slaamri@yahoo.fr>
2025-03-04 19:06:33 +0100

    6-states.sql

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- Script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on my MySQL server.
+-- Query that creates the database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Query that creates the table states.
+CREATE TABLE IF NOT EXISTS states (id INT PRIMARY KEY, VARCHAR(256) NOT NULL);
