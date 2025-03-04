0be011ddfa9832bf7c863baa1e57c87c1b44f2ec
SaLaam17 <slaamri@yahoo.fr>
2025-03-04 11:42:45 +0100

    10-top_score.sql

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,2 +1,3 @@
 -- Script that lists all records of the table second_table of the database hbtn_0c_0 in my MySQL server.
--- Query that lists all records of the table second_table of the database hbtn_0c_0 in my MySQL server.
+-- Query that displays both the score and the name, ordered by score (top first)
+SELECT score, name FROM second_table ORDER BY score DESC;
