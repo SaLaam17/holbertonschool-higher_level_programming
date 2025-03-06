10c326c58e58466c90b0b5e6c73877cc83921580
SaLaam17 <slaamri@yahoo.fr>
2025-03-06 23:26:20 +0100

    model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -18,7 +18,6 @@ class State(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
 
-
 if __name__ == "__main__":
     # Récupère les arguments passés dans la ligne de commande
     username = sys.argv[1]
@@ -26,9 +25,7 @@ if __name__ == "__main__":
     db_name = sys.argv[3]
 
     # Créer la connexion avec la base de données MySQL
-    engine = create_engine(
-        f'mysql+mysqldb://{username}:{
-            password}@localhost/{db_name}', pool_pre_ping=True)
+    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost/{db_name}', pool_pre_ping=True)
 
     # Créer toutes les tables liées à la base (table 'states' incluse)
     Base.metadata.create_all(engine)
