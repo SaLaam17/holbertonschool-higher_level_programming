e410f7fb38885e69497708b8f9d476e193433d64
SaLaam17 <slaamri@yahoo.fr>
2025-03-06 23:23:26 +0100

    fixing pycodestyle errors inside model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
+"""Start link class to table in database
 """
 import sys
 from sqlalchemy import create_engine, Column, Integer, String
@@ -7,6 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 # Déclaration de la base
 Base = declarative_base()
+
 
 # Définition de la classe State
 class State(Base):
@@ -17,6 +18,7 @@ class State(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
 
+
 if __name__ == "__main__":
     # Récupère les arguments passés dans la ligne de commande
     username = sys.argv[1]
@@ -24,7 +26,9 @@ if __name__ == "__main__":
     db_name = sys.argv[3]
 
     # Créer la connexion avec la base de données MySQL
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost/{db_name}', pool_pre_ping=True)
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{
+            password}@localhost/{db_name}', pool_pre_ping=True)
 
     # Créer toutes les tables liées à la base (table 'states' incluse)
     Base.metadata.create_all(engine)
