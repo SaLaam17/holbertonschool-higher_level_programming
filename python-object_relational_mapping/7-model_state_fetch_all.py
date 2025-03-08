0382a7ec2e268db6b08a9acfd498740548ef206b
SaLaam17 <slaamri@yahoo.fr>
2025-03-08 13:50:30 +0100

    7-model_state_fetch_all.

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+Script that lists all State objects from the database hbtn_0e_6_usa.
+"""
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import State
